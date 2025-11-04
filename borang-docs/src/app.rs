81ea39ec211a81032179d7f04bd06048943f3670
Jon Saw <jon.saw@gmail.com>
2025-11-04 20:11:56 +0800

    Add borang as dependency and basic email form example

--- borang-docs/src/app.rs
+++ borang-docs/src/app.rs
@@ -1,12 +1,30 @@
+use borang::{
+    rules::{Required, Rules},
+    Field, FieldErrorFor, Form, Input, UseFormValidation, WithMessage,
+};
 use leptos::prelude::*;
 use leptos_meta::*;
 
 #[component]
 pub fn App() -> impl IntoView {
     provide_meta_context();
+
+    let form = UseFormValidation::new();
+
+    let email = form
+        .field("email", String::new())
+        .with_validator(Rules::new().add(WithMessage::new(Required, "Email is required")));
+
     view! {
         <h1 class="text-3xl font-bold">
             "Borang"
         </h1>
+        <Form>
+            <Field field=email.clone()>
+                <Input attr:id="email" attr:r#type="email" attr:placeholder="Email" />
+                <FieldErrorFor field=email />
+            </Field>
+            <button type="submit">Submit</button>
+        </Form>
     }
 }
