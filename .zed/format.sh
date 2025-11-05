c1c3e019fae6b7870fc1b621ad8d18b26eecca4d
Jon Saw <jon.saw@gmail.com>
2025-11-05 18:46:17 +0800

    Setup save on format for Zed Editor

---
+++ .zed/format.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+leptosfmt --stdin | \
+    rustywind --stdin | \
+    rustywind --stdin --custom-regex "\b\w*class=(?:cn!\([^\"]*\")?([_a-zA-Z0-9\s\-:\[\]/\.]+)\"" | \
+    rustfmt --edition 2024
